be1fd4f89ede66474c4491fbd909cc0cc85e6ef3
Tao Bror Bojlén <tao@btao.org>
2020-03-04 16:36:29 +0000

    fix coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Wobbly.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.json": :test]
     ]
   end
 
